885ddbac276541f4459cf9e3217c24f43cee77fe
devJiraynor <jiraynor@naver.com>
2025-02-24 11:55:37 +0900

    2025.02.24. 11:50

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Component, { ClassComponent, FunctionalComponent } from './component/Component'
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <ClassComponent />
+      <FunctionalComponent />
+      <Component />
     </div>
   );
 }
